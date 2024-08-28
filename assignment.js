f73c501f1cd7540ca0ddf468284be4de0dcbf6df
Hasan <searchio200@gmail.com>
2024-08-28 20:11:03 +0600

    Home About Contact page

--- assignment.js
+++ assignment.js
@@ -4,6 +4,23 @@ let fs=require('fs');
 let express=require('express');
 const app=express();
 
+app.get("/",function (req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end("This is the Home Page")
+
+});
+app.get("/about",function (req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end("This is the About Page")
+
+});
+app.get("/contact",function (req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+
+    res.end("This is the Contact Page")
+});
+
+
 app.listen(5500,function (){
     console.log('Server started on port 5500');
 })
